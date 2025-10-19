95e3ce3fafdd425df0f2115660013b6b3cfe7af2
Oktay Dadar <oktaydadar@gmail.com>
2025-10-19 14:24:40 +0300

    ProductData eklendi.

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ProductCard from "./ProductCard";
 import "./Products.css";
-
+import ProductCardData from "./productsData.js";
 function Products() {
+  const image = ProductCardData[5].image;
+  const title = ProductCardData[0].title;
+  const price = ProductCardData[0].price;
+
   return (
     <div className="products">
       <h2>Products Component</h2>
 
       <div className="products-wrapper">
-        <ProductCard
-          image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png"
-          title="Çanta"
-          price="89"
-        />
+        <ProductCard image={image} title={title} price={price} />
         <ProductCard
           image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png"
           title="Çanta"
