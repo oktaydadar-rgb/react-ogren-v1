c1d4cd9960b3f515489390e2f0566e0ddff57e63
Oktay Dadar <oktaydadar@gmail.com>
2025-10-27 14:49:48 +0300

    coklu state , coklu function_ form_dinamik_ornek_kod kopyasi

---
+++ src/components/Products/ProductCard_coklu_state_form_dinamik.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./ProductCard.css";
+function ProductCard(props) {
+  const image = props.image;
+
+  const price = props.price;
+  const description = props.description;
+
+  function handleTitleChange() {
+    props.setTitleState("Title Değişti");
+  }
+
+  return (
+    <div className="product-card">
+      <img className="product-image" src={image} alt="product image" />
+      <div className="product-info">
+        <strong className="product-title">{props.titleState}</strong>
+        <span className="product-price">{price}</span>
+        <p className="product-description">{description}</p>
+
+        <button onClick={handleTitleChange}> Sepete Ekle</button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
---
+++ src/components/Products/Products_coklu_state_form_dinamik.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import ProductCard from "./ProductCard";
+import "./Products.css";
+import ProductCardData from "../../data/productsData.js";
+import AddProductForm from "./AddProductForm.jsx";
+function Products() {
+  const [titleState, setTitleState] = useState("Title");
+  return (
+    <div className="products">
+      <h2>Add Product Component</h2>
+      <AddProductForm />
+      <h2>Products Component</h2>
+      <div className="products-wrapper">
+        {ProductCardData.map((product, index) => {
+          return (
+            <ProductCard
+              key={product.id ?? index}
+              image={product.image}
+              title={product.title}
+              price={product.price}
+              description={product.description}
+              titleState={titleState}
+              setTitleState={setTitleState}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Products;
